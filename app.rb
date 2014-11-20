c7f1a79c5fe955ed9f084ee916c5110f8ed5cada
Kwabena Boateng <kboateng3@gmail.com>
2014-11-20 16:13:02 +0000

    update

--- app.rb
+++ app.rb
@@ -11,23 +11,6 @@ end
 
 
 post '/your-stock-price' do
-  # Convert input to CAPS
-#   data_table = GoogleVisualr::DataTable.new
-#   # Add Column Headers
-# data_table.new_column('string', 'Year' )
-# data_table.new_column('number', 'Sales')
-# data_table.new_column('number', 'Expenses')
-
-# # Add Rows and Values
-# data_table.add_rows([
-#     ['2004', 1000, 400],
-#     ['2005', 1170, 460],
-#     ['2006', 660, 1120],
-#     ['2007', 1030, 540]
-# ])
-
-# option = { width: 400, height: 240, title: 'Company Performance' }
-# @chart = GoogleVisualr::Interactive::AreaChart.new(data_table, option)
 
 @today = DateTime 
   @input = params[:symbol].upcase
